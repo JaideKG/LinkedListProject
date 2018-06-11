37e2fb31ff4962c244d685ea99e31c4d0555d8cf
Jaide King-Griffin <jaidekg@gmail.com>
2018-06-10 21:21:43 -0400

    updated

--- LinkedListProject/Program.cs
+++ LinkedListProject/Program.cs
@@ -10,14 +10,14 @@ namespace LinkedListProject
     {
         static void Main(string[] args)
         {
-            int[] TestArray = new int[11] { 4, 5, 3, 2, 1, 6, 7, 8, 9, 1, 9 };
+            int[] TestArray = new int[11] { 4, 5, 456, 2, 1, 6,456, 8, 9, 1, 9 };
             int[] Frequency = new int[11];
             Frequency = LinkedList.methodCount(TestArray);
 
             for (int j = 0; j < Frequency.Length; j++)
             {
-                Console.WriteLine($"[{j}]: {Frequency[j]}");
-            }
+				Console.WriteLine($"{TestArray[j]} appears {Frequency[j]} time(s) in your array.");
+			}
 
             LinkedList list = new LinkedList();
             list.AddAtStart("Seahorse");
